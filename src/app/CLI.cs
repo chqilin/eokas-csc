10e5e3c52d0d4c9d7020494f03c4f0e510fc7bb8
Qilin <qilin.cheng@foxmail.com>
2023-03-09 17:55:58 +0800

    Feat (App): add CLI and app framework

---
+++ src/app/CLI.cs
@@ -0,0 +1,174 @@
+﻿namespace Eokas;
+
+public class Option
+{
+    public String name = "";
+    public String info = "";
+    public string value = "false";
+
+    public String toString()
+    {
+	    return String.Format("\t{0}\t\t\t\t{1} (default:{2})\n", name, info, value);
+    }
+};
+
+public class Command
+{
+	public delegate void Func(Command cmd);
+
+	public String name;
+	public String info;
+	public Dictionary<String, Option> options;
+	public Func func;
+
+	public Dictionary<String, Command> subCommands;
+
+	public Command()
+	{
+		this.name = "";
+		this.info = "";
+		this.options = new Dictionary<string, Option>();
+		this.func = null;
+		this.subCommands = new Dictionary<string, Command>();
+	}
+	
+	public Command(String name, String info = "")
+	{
+		this.name = name;
+		this.info = info;
+	}
+		
+	public Command Option(String name, String info, String defaultValue)
+	{
+		Option opt = new Option();
+		opt.name = name;
+		opt.info = info;
+		opt.value = defaultValue;
+		this.options.Add(name, opt);
+	
+		return this;
+	}
+
+	public Command Action(Func func)
+	{
+		this.func = func;
+		return this;
+	}
+
+	public Command SubCommand(String name, String info)
+	{
+		Command cmd = new Command(name, info);
+		this.subCommands.Add(name, cmd);
+		return cmd;
+	}
+
+	public String FetchValue(String shortName)
+	{
+		var opt = this.FetchOption(shortName);
+		if(opt == null)
+			return "";
+		return opt.value;
+	}
+
+	public Option? FetchOption(String shortName)
+	{
+		foreach(var item in this.options)
+		{
+			var fragments = item.Key.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+			if (fragments.Contains(shortName))
+				return item.Value;
+		}
+		return null;
+	}
+
+	public Command? FetchCommand(String shortName)
+	{
+		foreach(var item in this.subCommands)
+		{
+			var fragments = item.Key.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+			if (fragments.Contains(shortName))
+				return item.Value;
+		}
+		
+		return null;
+	}
+
+	public String toString()
+	{
+		String str = String.Format("{0}\t\t\t\t{1}\n", name, info);
+		foreach(var opt in this.options)
+		{
+			str += opt.Value.toString();
+		}
+		foreach(var cmd in this.subCommands)
+		{
+			str += cmd.Value.toString();
+		}
+		return str;
+	}
+
+	public void Execute(string[] args)
+	{
+		if (args == null || args.Length == 0 || this.name != args[0])
+			throw new Exception("Invalid Arguments");
+		
+		bool isArgumentsConsumedByCommands = false;
+		bool isArgumentsConsumedByOptions = false;
+	
+		// process sub-commands.
+		if(args.Length > 1 && this.subCommands.Count > 0)
+		{
+			String cmdName = args[1];
+			var cmd = this.FetchCommand(cmdName);
+			if (cmd != null)
+			{
+				isArgumentsConsumedByCommands = true;
+				
+				string[] subArgs = new string[args.Length - 1];
+				for (int i = 0; i < subArgs.Length; i++)
+				{
+					subArgs[i] = args[i + 1];
+				}
+				
+				cmd.Execute(subArgs);
+			}
+		}
+	
+		// process options.
+		if(args.Length > 1 && this.options.Count > 0)
+		{
+			foreach(var opt in this.options)
+			{
+				// compatible with "-v,--version"
+				var fragments = opt.Key.Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+				for (int i = 0; i < fragments.Length; i++)
+				{
+					var frag = fragments[i];
+					if (!args.Contains(frag))
+						continue;
+
+					int next = i + 1;
+
+					opt.Value.value = next == fragments.Length || fragments[next].StartsWith("-")
+						? "true"
+						: fragments[next];
+					
+					isArgumentsConsumedByOptions = true;
+				}
+			}
+		}
+	
+		// Only has commands but arguments were not consumed by commands.
+		if(this.subCommands.Count > 0 && this.options.Count <= 0 && !isArgumentsConsumedByCommands)
+			throw new Exception("Invalid Arguments.");
+
+		// Has options but arguments didn't consumed by options.
+		if(this.options.Count > 0 && !isArgumentsConsumedByOptions)
+			throw new Exception("Invalid arguments");
+
+		if (this.func != null)
+		{
+			this.func(this);
+		}
+	}
+};
---
+++ src/app/Program.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Eokas
+{
+    using LLVMSharp;
+    
+    internal class Program
+    {
+        private static string EOKAS_VERSION = "0.0.1";
+        
+        static int Main(string[] args)
+        {
+            Command program = new Command(args[0]);
+            program.Action((cmd) =>
+            {
+                About();
+            });
+            program.SubCommand("help", "")
+                .Action((cmd)=>
+                {
+                    Help();
+                });
+	
+            program.SubCommand("compile", "")
+                .Option("--file,-f", "", "")
+                .Action((cmd)=>
+                {
+                    var file = cmd.FetchValue("--file");
+                    if(file == "")
+                        throw new Exception("The argument 'file' is empty.");
+                    
+                    Console.WriteLine("=> Source file: {0}", file);
+			
+                    //eokas_main(file, llvm_aot);
+                });
+
+            program.SubCommand("run", "")
+                .Option("--file,-f", "", "")
+                .Action((cmd) =>
+                {
+                    var file = cmd.FetchValue("--file");
+                    if(file == "")
+                        throw new Exception("The argument 'file' is empty.");
+                    
+                    Console.WriteLine("=> Source file: {0}", file);
+                    
+                    // eokas_main(file, llvm_jit);
+                });
+		
+            try
+            {
+                program.Execute(args);
+                return 0;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR: {0}", e.Message);
+                return -1;
+            }
+        }
+
+        static void EokasMain(string fileName)
+        {
+            /*
+            static void eokas_main(const String& fileName, bool(*proc)(ast_node_module_t* m))
+            {
+                FileStream in(fileName, "rb");
+                if(!in.open())
+                return;
+	
+                size_t size = in.size();
+                MemoryBuffer buffer(size);
+                    in.read(buffer.data(), buffer.size());
+                    in.close();
+	
+                String source((const char*) buffer.data(), buffer.size());
+                printf("=> Source code:\n");
+                printf("------------------------------------------\n");
+                printf("%s\n", source.replace("%", "%%").cstr());
+                printf("------------------------------------------\n");
+	
+                parser_t parser;
+                ast_node_module_t* m = parser.parse(source.cstr());
+                printf("=> Module AST: %llX\n", (u64_t)m);
+                if(m == nullptr)
+                {
+                    const String& error = parser.error();
+                    printf("ERROR: %s\n", error.cstr());
+                    return;
+                }
+	
+                FileStream out(String::format("%s.ll", fileName.cstr()), "w+");
+                if(!out.open())
+                return;
+	
+                printf("=> Encode to IR:\n");
+                printf("------------------------------------------\n");
+                proc(m);
+                printf("------------------------------------------\n");
+                    out.close();
+            }
+            */
+        }
+        
+        static void About()
+        {
+            Console.WriteLine("eokas {0}\n", EOKAS_VERSION);
+        }
+
+        static void Help()
+        {
+            Console.Write(@"
+-?, -help
+\tPrint command line help message.
+fileName [-c] [-e] [-t]
+\tComple or Execute a file, show exec-time.
+"
+            );
+        }
+    }
+}
